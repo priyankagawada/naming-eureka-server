f11f19709df60c79ce1eaede3a57ae31bcd937a7
john <john@gmail.com>
2021-05-14 13:49:06 +0530

    Initial Commit

---
+++ src/test/java/com/training/boot/patientsystemeurekaserver/PatientSystemEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.boot.patientsystemeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSystemEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
